a3d2620806778ad25f6775f48f72a8877e0ab079
aiurti1986 <35649031+aiurti1986@users.noreply.github.com>
2018-01-21 20:12:21 +0900

    Add files via upload

---
+++ nanj/resources/lang/ch/passwords.php
@@ -0,0 +1,19 @@
+
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => '请把密码设定6位以上，并与确认栏一致。',
+    'reset' => '重新设定密码成功！',
+    'sent' => '重新设定密码的链接已发送。',
+    'token' => '重新设定密码用的通证不正确。',
+    'user' => "利用这个邮箱地址的用户不存在。",
+];
